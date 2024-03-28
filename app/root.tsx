ec5b3a2068306cd2e8b3513f672bdc2a186c0ba6
rossipedia <bryan.ross@docker.com>
2024-03-27 18:26:33 -0600

    Initial commit from create-remix

---
+++ app/root.tsx
@@ -0,0 +1,42 @@
+import { ThemeProvider, createTheme } from '@mui/material';
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  const theme = createTheme({
+    components: {
+      MuiTypography: {
+        defaultProps: {
+          color: 'green',
+        },
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </ThemeProvider>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import type { MetaFunction } from '@remix-run/node';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
+  ];
+};
+
+export default function Index() {
+  return <Typography variant="h1">Welcome to Remix</Typography>;
+}
